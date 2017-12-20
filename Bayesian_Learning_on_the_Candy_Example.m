bacdabffbac633ff1191fb78f8afdf3f161ab051
dymodi <dymodi@gmail.com>
2017-12-20 18:06:14 +0800

    add Bayesian m code

---
+++ Bayesian_Learning_on_the_Candy_Example.m
@@ -0,0 +1,33 @@
+%% This is an auxiliary code for blog
+% "Bayesian Learning on the Candy Example"
+% 12/20/17
+% Yi DING
+
+
+%% Bayesian updating
+clc;clear;
+p_h = [0.1;0.2;0.4;0.2;0.1];
+p_lime = [0;0.25;0.5;0.75;1];
+
+num_candy_drawn = 10;
+% Inintialization
+p_h_d = zeros(num_candy_drawn+1,5);
+% Updating
+for i = 0:num_candy_drawn
+    p_d_h = (p_lime.^i);
+    p_d = p_h'* p_d_h;
+    p_h_d(i+1,:) = (p_h.*p_d_h)'/p_d;
+end
+
+%% Plot
+figure
+ftsize = 20;
+plot(0:10,p_h_d,'LineWidth',2);
+set(gca,'fontsize',ftsize);
+grid on;
+title('Bayesian updating for the candy bag example','FontSize',ftsize)
+xlabel('# of samples in d','FontSize',ftsize);
+ylabel('Posterior probability of hypothesis','FontSize',ftsize)
+leg1 = legend('$P(h_1|d)$','$P(h_2|d)$','$P(h_3|d)$','$P(h_4|d)$',...
+    '$P(h_5|d)$');
+set(leg1,'Interpreter','latex','FontSize',ftsize);
