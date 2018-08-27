c889efbbbaa6d0d540f13332128a23ba174ad976
Yi Ding <yi.ding02@ele.me>
2018-08-27 17:14:02 +0800

    add

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='machine-learning',  # Required
+    name='ml',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
